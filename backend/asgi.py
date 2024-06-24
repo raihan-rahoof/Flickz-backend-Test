9fc546d389dd9d3e6e04dc4520e2ee72379b1a44
raihan-rahoof <mcrahoof28@gmail.com>
2024-06-24 17:43:25 +0530

    added django setup in asgi.py file to ressolce apploading issue

--- backend/asgi.py
+++ backend/asgi.py
@@ -3,9 +3,10 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import theatre_screen.routing
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-
+django.setup()
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
